1d560411dc7ecde988174bb340e345cd28c4ca47
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-10-02 22:28:51 -0600

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,60 @@
+public class Cliente {
+    // Atributos
+    private int id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    // Constructor
+    public Cliente(int id, String nombre, String direccion, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    // Métodos
+    public void realizarPedido(Pedido pedido) {
+        System.out.println(nombre + " ha realizado un nuevo pedido.");
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Cliente ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
